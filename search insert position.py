4583e9808ee0e7539dfeee63d0b13f2d00960402
starkcod3 <98265463+starkcod3@users.noreply.github.com>
2022-11-08 13:29:14 +0000

    Add files via upload

---
+++ search insert position.py
@@ -0,0 +1,11 @@
+class Solution(object):
+	def searchInsert(self, nums, target):
+
+		if target > nums[-1]:
+			return len(nums)
+
+		for index, num in enumerate(nums):
+			if num == target:
+				return index
+			elif target < num:
+				return index
